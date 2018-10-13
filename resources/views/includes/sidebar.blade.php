166daf1a69dae7e7bc133a3af8e96f375672b4bc
anupama <ak2522@njit.edu>
2018-10-13 15:52:14 -0400

    Added sidebar.blade.php in includes folder

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,7 @@
+<nav id="sidebar-nav">
+    <ul class="nav nav-pills nav-stacked">
+        <li><a href="#">Contact us</a></li>
+        <li><a href="#">More Information</a></li>
+        <li><a href="#">Gallery</a></li>
+    </ul>
+</nav>

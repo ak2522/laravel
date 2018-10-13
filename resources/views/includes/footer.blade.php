1e116847ac3a7e415b54b0ecccae9e4b4d3f14d5
anupama <ak2522@njit.edu>
2018-10-13 16:33:32 -0400

    Changes in footer.blade.php

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,5 @@
 <footer class="container">
-<p class = "float-right">
-    <a href ="#">Return to Top</a>
-</p>
+<p class="float-right"><a href ="#">Return to Top</a></p>
 <div id="copyright text-right">© Copyright 2015 Frozen Artists
 <a href ="#">Terms and Conditions</a>
 </div>

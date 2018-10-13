61512c753eea117a0e299e6cb53fd18a61a3663e
anupama <ak2522@njit.edu>
2018-10-13 16:51:45 -0400

    Added some css links in head.blade.php for styling

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -6,3 +6,6 @@
 
 <!-- load bootstrap from a cdn -->
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+<link rel="stylesheet" href="https://>/maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/>dAiS6JXm" crossorigin="anonymous">
+<link href="/css/app.css" rel="stylesheet">

f5b59d612ca1a58031a4157c334cfce5faa12767
anupama <ak2522@njit.edu>
2018-10-14 13:50:56 -0400

    Change navigations in header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,14 +1,22 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <a class="navbar-brand" href="#">Laravel</a>
-    </nav>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
 
-<div class="navbar">
-    <div class="navbar-inner">
-        <ul class="navbar-inline">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
+<div class="collapse-navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a></li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a></li>
         </ul>
+    <form class="form-inline mt-2 mt-md-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        <button class="btn-btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
     </div>
-</div>
+</nav>
 </header>

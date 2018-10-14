6e7dc5a092f02c92c79f81fe6662d6b875979d5d
anupama <ak2522@njit.edu>
2018-10-14 15:27:27 -0400

    Added a paragraph in home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.default')
 @section('content')
+    <div class="container">
     <h1>Welcome to my Bookstore</h1>
     <h2> A place of quality content and quantity knowledge</h2>
+     <p>Explore and learn</p>
+    </div>
 @stop

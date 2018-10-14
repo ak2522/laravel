1ab5ab985361c26961099be6136dfb676ca3d838
anupama <ak2522@njit.edu>
2018-10-14 12:01:27 -0400

    Changes in header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,13 +1,13 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a id="logo" href="/">Laravel</a>
+    <a class="navbar-brand" href="#">Laravel</a>
     </nav>
 
 <div class="navbar">
     <div class="navbar-inner">
         <ul class="navbar-inline">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
         </ul>
     </div>
 </div>

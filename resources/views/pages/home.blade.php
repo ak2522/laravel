92f7f32e74422da6fde540bc08e3a456d9b753d3
anupama <ak2522@njit.edu>
2018-10-14 15:00:36 -0400

    Added header2 in home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.default')
 @section('content')
     <h1>Welcome to my Bookstore</h1>
-    <p> A place of quality content and quantity knowledge</p>
-    <p>Explore and Learn</p>
+    <h2> A place of quality content and quantity knowledge</h2>
 @stop
